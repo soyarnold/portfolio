9d990b27bcf78b5137d181dd2d61b7fa3e781b4a
Arnold Perez <arnolddperez@gmail.com>
2021-11-13 14:01:56 -0600

    changed technology section

--- components/Technologies.js
+++ components/Technologies.js
@@ -1,37 +1,37 @@
-import { FaReact, FaDatabase, FaTools } from "react-icons/fa";
+import { FaReact, FaDatabase, FaTools } from 'react-icons/fa'
 
-import TechItem from "./TechItem";
+import TechItem from './TechItem'
 
 export default function Technologies() {
   return (
-    <section id="technologies" className="my-20">
-      <h1 className="text-2xl lg:text-5xl font-bold tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-600">
+    <section id='technologies' className='my-20'>
+      <h1 className='text-2xl lg:text-5xl font-bold tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-600'>
         Technologies
       </h1>
-      <p className="text-gray-400 mt-8">
+      <p className='text-gray-400 mt-8'>
         I've worked with a range of technologies in the web development world
         from back-end to front-end.
       </p>
-      <div className="flex flex-col md:flex-row md:justify-evenly">
+      <div className='flex flex-col md:flex-row md:justify-evenly'>
         <TechItem
           Icon={FaReact}
-          title="Front-End"
-          experience="Experience with"
-          list="React.js, React Native and Jakarta Server Pages"
+          title='Front-End'
+          experience='Experience with'
+          list='React.js and React Native'
         />
         <TechItem
           Icon={FaDatabase}
-          title="Back-End"
-          experience="Experience with"
-          list="Django, Node.js, Java EE, and PostgreSQL"
+          title='Back-End'
+          experience='Experience with'
+          list='Spring Boot, Django, Node.js, and Databases'
         />
         <TechItem
           Icon={FaTools}
-          title="UI/UX"
-          experience="Experience with"
-          list="tools like Tailwind and Material-UI"
+          title='UI/UX'
+          experience='Experience with'
+          list='tools like Tailwind and Material-UI'
         />
       </div>
     </section>
-  );
+  )
 }
