53528222a595846bc75c150d4835d57580fb70e1
Arnold Perez <arnolddperez@gmail.com>
2021-11-13 13:55:55 -0600

    removed about me section

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,18 @@
 export default function Hero() {
   return (
-    <section className="my-20 space-y-7">
-      <h1 className="text-3xl lg:text-5xl font-bold tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-600">
+    <section className='my-20 space-y-7'>
+      <h1 className='text-3xl lg:text-5xl font-bold tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-600'>
         Welcome To <br /> My Personal Portfolio
       </h1>
-      <p className="font-hero">
+      <p className='font-hero'>
         Hello! My name is Arnold Perez and I am a full stack developer based in
-        Dallas, TX. I mostly code in Java, JavaScript, and Python.
+        Dallas, TX.
       </p>
-      <a href="https://github.com/soyarnold" target="blank" className="block">
-        <button className="rounded-3xl py-2 px-20 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 focus:outline-none">
+      <a href='https://github.com/soyarnold' target='blank' className='block'>
+        <button className='rounded-3xl py-2 px-20 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 focus:outline-none'>
           Learn More
         </button>
       </a>
     </section>
-  );
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,29 @@
-import Head from "next/head";
-import About from "../components/About";
-import Dropdown from "../components/Dropdown";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Projects from "../components/Projects";
-import Technologies from "../components/Technologies";
+import Head from 'next/head'
+import About from '../components/About'
+import Dropdown from '../components/Dropdown'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Projects from '../components/Projects'
+import Technologies from '../components/Technologies'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className=''>
       <Head>
         <title>Arnold Perez</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div className="relative p-6 max-w-screen-xl mx-auto">
+      <div className='relative p-6 max-w-screen-xl mx-auto'>
         <Header />
         <Dropdown />
         <Hero />
         <Projects />
         <Technologies />
-        <About />
+        {/* <About /> */}
         <Footer />
       </div>
     </div>
-  );
+  )
 }
